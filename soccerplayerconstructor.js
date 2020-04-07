bb7a24c74f08d60c0ad5b46e0b06bd4d5693580e
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-07 16:44:56 -0400

    created constructor function for player bio

---
+++ soccerplayerconstructor.js
@@ -0,0 +1,7 @@
+function SoccerPlayers(name, position, number, age, team) {
+    this.name = name;
+    this.position = position;
+    this.number = number;
+    this.age = age;
+    this.team = team;
+}
